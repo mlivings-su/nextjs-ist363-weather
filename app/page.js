3c1507ed8a67d96d2ebe73aa7795afca88afa3e2
mlivings-su <123092458+mlivings-su@users.noreply.github.com>
2024-04-11 18:24:27 -0400

    4/11

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 import ButtonDemo from "../components/ButtonDemo";
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "../components/PeoplePicker";
+import Tabs from "../components/Tabs";
 
 import {
   getGeoLocation,
@@ -32,9 +33,9 @@ const Homepage = () => {
       })
       .catch((error) => {
         setErrorMsg(error);
-        // console.log(error);
       });
   }, []);
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await getWeatherDataByLatLon(location);
@@ -52,28 +53,24 @@ const Homepage = () => {
         const date = new Date(block.dt * 100);
         const options = { weekday: "short" };
         const day = date.toLocaleDateString("en-US", options);
-        console.log(day);
+        // console.log(day);
+        if {!tempWeek.includes(day)} {
+          tempWeek.push(day);
+        }
       });
 
     setDaysOfWeek(tempWeek);
+
     //then set state with the day of the week
   }, [weatherData]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await getWeatherData();
-  //     setWeatherData(response);
-  //   };
-  //   fetchData();
-  // }, []);
-
-  //console.log({ peopleArr });
   return (
     <div>
       <h1> Weather App </h1>
       (errorMsg && <div>{errorMsg}</div>)
       {weatherData && (
         <div>
+          
           <h2>{weatherData.city.name}</h2>
           <p>Current temp: {weatherData.list[0].main.temp}&deg; F</p>
           <p>{weatherData.list[0].weather[0].description}</p>
@@ -90,18 +87,31 @@ const Homepage = () => {
             height={100}
           />
         </div>
+       
       )}
+
       {/* <PeoplePicker people={peopleArr} />
       <ButtonDemo />
       <ColorPicker /> */}
       {daysOfWeek && (
+        <section>
+         <Tabs 
+         activeIndex={activeDayIndex}
+         items={daysOfWeek} clickHandler={setActiveDayIndex}/>
         <div>
-          {daysOfWeek?.map((day, index) => {
-            return <li key={index}>{day.main.temp}</li>;
-          })}
+          {weatherData?.list
+          .filter((block) => {
+          const date = new Date(block.dt * 1000);
+          const options = {weekday: "short"};
+          const day = date.toLocaleDateString("en-US", options);
+          return day === daysOfWeek[activeDayIndex];
+        })
+        .map((block,index)) => {
+          return <p key={index}>{block.main.temp}></p>
+      }
         </div>
-      )}
-    </div>
-  );
-};
+    </section>
+      );
+    };
+
 export default Homepage;
