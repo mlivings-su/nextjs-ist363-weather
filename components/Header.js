0a7f5e43613dc75a887c6e4e8f2ffdaf1ee90105
mlivings-su <123092458+mlivings-su@users.noreply.github.com>
2024-04-18 18:32:56 -0400

    4/18

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,12 @@
-import { Container } from "./Container";
+import Container from "./Container";
+
 import styles from "./Header.module.scss";
 
 const Header = () => {
-  return <header className={styles.header}>Logo and nav</header>; //fix--> need header
+  return (
+    <header className={styles.header}>
+      <Container>Logo and nav</Container>
+    </header>
+  );
 };
 export default Header;
